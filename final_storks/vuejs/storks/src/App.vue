5f652a15ac22bf99403d9a020929f5488af6f352
Adam Hocini <adam@MacBook-Air-de-Adam.local>
2021-04-05 17:13:45 +0200

    stoks

---
+++ final_storks/vuejs/storks/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+}
+#app {
+  height: 100%;
+}
+.app {
+  height: 100%;
+}
+.grey-bg {  
+    background-color: #F5F7FA;
+}
+
+</style>
+
---
+++ final_storks/vuejs/storks/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Data />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Data from "@/components/Data.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Data,
+  },
+};
+</script>
